4c25ef9366335221521adc49d0a04cc20525543f
john <john_wilkinson3@sky.com>
2013-01-11 15:27:01 +0000

    Tried to get another controller method to work

--- ClipViewer/src/main/java/com/vidlib/clipviewer/HomeController.java
+++ ClipViewer/src/main/java/com/vidlib/clipviewer/HomeController.java
@@ -10,10 +10,13 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Handles requests for the application home page.
@@ -23,7 +26,18 @@ public class HomeController {
 
 	private static final Logger logger = LoggerFactory
 			.getLogger(HomeController.class);
-
+	
+	@RequestMapping(value = "/thumbnails", 
+            method = ( RequestMethod.GET ), 
+            produces = MediaType.APPLICATION_JSON_VALUE )
+	public @ResponseBody String listJSON(@RequestParam int first , @RequestParam int last )
+	{
+		logger.info("Getting thumbnails");
+		
+		return "hello";
+	}
+	
+	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
