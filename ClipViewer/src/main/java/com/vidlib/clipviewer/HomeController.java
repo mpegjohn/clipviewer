f2d2dc46310dbe9517351346be2cc128976b6fcf
mpegjohn <john_wilkinson3@sky.com>
2014-01-03 15:37:04 +0000

    Added some comments

--- ClipViewer/src/main/java/com/vidlib/clipviewer/HomeController.java
+++ ClipViewer/src/main/java/com/vidlib/clipviewer/HomeController.java
@@ -38,6 +38,11 @@ import com.vidlib.service.jpa.MediaServicejpa;
 /**
  * Handles requests for the application home page.
  */
+/**
+ * @author john
+ *
+ */
+
 @Controller
 public class HomeController {
 	
@@ -53,11 +58,33 @@ public class HomeController {
 	@Autowired
 	private WebApplicationContext context;
 
+	/**
+	 * currentCarouselPage is a Page collection that holds a list of scenes for the current 
+	 * selected page.
+	 */
 	private Page<Scene> currentCarouselPage;
 	
 	private static final Logger logger = LoggerFactory
 			.getLogger(HomeController.class);
-	
+	/**
+	 * The UI requests a specific page of caraousels to show. It foes this by setting the page
+	 * number and the number of scenes /carousels to display.
+	 * This method thatn retrieves the correct list of scenes for tis page.
+	 * <p>
+	 * For each scene it must construct the correct URL to retrieve the thumbnails.
+	 * It does this by using the filestore  AddThumbnailsUrls method, that adds the list of 
+	 * Thumbnail URL's to each scene. This is stored in the currentCarouselPage.
+	 * <p>
+	 * The CarouselPanePage returned contains a list of scene ID's and the number of pages.
+	 * This allows the UI to create the correct number of carousels and give each the correct scene ID
+	 * As well as setting up the pagination control.
+	 * 
+	 * @param mediaId The selected media ID to show scenes from
+	 * @param page The current page number
+	 * @param size The number of scenes in this page
+	 * @return A JSON object derived from CarouselPanePage class
+	 * @see CarouselPanePage
+	 */
 	@RequestMapping(value = "/media/{id}", method = (RequestMethod.GET), produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody
 	CarouselPanePage getCrouselPage(@PathVariable("id") long mediaId, @RequestParam int page, @RequestParam int size)
@@ -65,11 +92,13 @@ public class HomeController {
 		page = page -1;
 		PageRequest pageRequest = new PageRequest(page, size);
 		
+		// Get all the scenes for this media and page 
 		this.currentCarouselPage = sceneService.FindByMediaIdPageable(mediaId, pageRequest);
 		
 		CarouselPanePage carouselPane = new CarouselPanePage();
 		
-		//for(int i = 0; i< this.currentCarouselPage.getNumberOfElements(); i++)
+		// iterate over the scenes and add the thumbnail url to each,
+		// also for each scene get its ID and add to the list of id's for this page.
 		for(Scene scene : this.currentCarouselPage)
 		{
 			//Scene scene = this.currentCarouselPage.getContent().get(i);
@@ -81,7 +110,14 @@ public class HomeController {
 		return carouselPane;
 	}
 	
-	
+	/**
+	 * When a carousel comes to load data, it calls this method, to get a list of thumbnails
+	 * and a total.
+	 * @param first The number of the first thumbnail to get
+	 * @param last the number of the last thumbnail to get
+	 * @param id the scene ID to which the thumbnails belong
+	 * @return a JSON object that is a list of thumbnails and a total.
+	 */
 	@RequestMapping(value = "/thumbnails", method = (RequestMethod.GET), produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody
 	ThumbnailItemList listJSON(@RequestParam int first, @RequestParam int last, @RequestParam long id) {
@@ -89,25 +125,30 @@ public class HomeController {
 		logger.info("Getting thumbnails");
 		logger.info("First:" + first + " last:" + last + " id:" + id);
 
-		//long id = idin;
-
+		// Get all the thumbnails for this scene
 		List<Thumbnail> thumbs = getThumbnailsFromPage(id);
-		
+
+		// Ensure the last item does not go past the end of the list
 		if(last > thumbs.size()-1)
 		{
 			last = thumbs.size()-1;
 		}
 		
+		// We only want a subset that we need to display.
 		List<Thumbnail> slice = thumbs.subList(first-1, last);
 
 		ThumbnailItemList theList = new ThumbnailItemList();
 		
+		// Create the object to send back to the UI
 		theList.setThumbnailList(slice);
 		theList.setTotal(thumbs.size());
 
 		return theList;
 	}
 
+	/**
+	 * 
+	 */
 	@RequestMapping(value = "/images/**", method = RequestMethod.GET)
 	public void getFileFromStore(HttpServletRequest request, HttpServletResponse response) {
 		UrlPathHelper helper = new UrlPathHelper();
