5173aa920f3293ae46722a210a2be9f019bbc752
john wilkinson <john_wilkinson3@sky.com>
2014-02-20 19:27:44 +0000

    Split out the unit tests

--- ClipViewer/src/test/java/com/vidlib/domain/ThumbnailTest.java
+++ ClipViewer/src/test/java/com/vidlib/domain/ThumbnailTest.java
@@ -2,33 +2,58 @@ package com.vidlib.domain;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
-
 
 public class ThumbnailTest {
 
-	@Test
-
-	public void testThumbnail() {
-		Thumbnail thumbnail = new Thumbnail();
-		Scene scene = new Scene();
-		
-		thumbnail.setId_thumbnail(1l);
-		thumbnail.setImageOrderNumber(789);
-		thumbnail.setImageSize(2048);
-		thumbnail.setImageTime("10.0.45");
-		thumbnail.setImage("The image");
-		thumbnail.setPath("/the/path");
-		thumbnail.setScene(scene);
-		
-		assertEquals(1l, thumbnail.getId_thumbnail());
-		assertEquals(789,thumbnail.getImageOrderNumber());
-		assertEquals(2048, thumbnail.getImageSize());
-		assertEquals("10.0.45", thumbnail.getImageTime());
-		assertEquals("The image", thumbnail.getImage());
-		assertEquals("/the/path", thumbnail.getPath());
-		assertEquals(scene, thumbnail.getScene());
-		
+	Thumbnail thumbnail;
+	
+	@Before
+	public void setup() {
+		this.thumbnail = new Thumbnail();
 	}
-
+	
+	@Test
+	public void testScene() {
+		Scene scene = new Scene();
+		thumbnail.setScene(scene);
+		assertEquals(scene, thumbnail.getScene());
+	}
+	
+	@Test
+	public void testId_thumbnail() {
+		thumbnail.setId_thumbnail(1l);
+		assertEquals(1l, thumbnail.getId_thumbnail());
+	}
+	
+	@Test
+	public void testImageOrderNumber() {
+		thumbnail.setImageOrderNumber(789);
+		assertEquals(789,thumbnail.getImageOrderNumber());
+	}
+	
+	@Test
+	public void testImageSize() {
+		thumbnail.setImageSize(2048);
+		assertEquals(2048, thumbnail.getImageSize());
+	}
+	
+	@Test
+	public void testImageTime() {
+		thumbnail.setImageTime("10.0.45");
+		assertEquals("10.0.45", thumbnail.getImageTime());
+	}
+	
+	@Test
+	public void testImage() {
+		thumbnail.setImage("The image");
+		assertEquals("The image", thumbnail.getImage());
+	}
+	
+	@Test
+	public void testPath() {
+		thumbnail.setPath("/the/path");
+		assertEquals("/the/path", thumbnail.getPath());
+	}
 }
